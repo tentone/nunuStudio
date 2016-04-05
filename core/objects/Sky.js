acf7f4bc995d847eb3841841fdbe5f443905d7a7
José Ferrão <joseferrao@ua.pt>
2016-04-05 02:45:55 +0100

    Sky finished Leap gestures and Editor position tool fixed

--- core/objects/Sky.js
+++ core/objects/Sky.js
@@ -23,8 +23,8 @@ function Sky()
 
 	//Day time
 	this.update_time = true;
-	this.day_time = 10; //seconds
-	this.time = 2.5;
+	this.day_time = 20; //seconds
+	this.time = 0;
 
 	//Sky Shader
 	var vertex = App.readFile("data/shaders/sky_vertex.glsl");
@@ -46,12 +46,13 @@ function Sky()
 	this.add(this.sky);
 }
 
-//Sky color
-Sky.color_top = [new THREE.Color(1.0, 0.0, 0.0), new THREE.Color(0.0, 1.0, 0.0), new THREE.Color(0.0, 0.0, 1.0), new THREE.Color(0.0, 0.46, 1.0)];
-Sky.color_bottom = [new THREE.Color(1.0, 1.0, 1.0), new THREE.Color(1.0, 1.0, 1.0), new THREE.Color(1.0, 1.0, 1.0), new THREE.Color(1.0, 1.0, 1.0)];
+//Sky color (morning, noon, afternoon, night)
+Sky.color_top = [new THREE.Color(0x77b3fb), new THREE.Color(0x0076ff), new THREE.Color(0x035bb6), new THREE.Color(0x002439)];
+Sky.color_bottom = [new THREE.Color(0xebece6), new THREE.Color(0xffffff), new THREE.Color(0xfee7d7), new THREE.Color(0x0065a7)];
 
 //Auxiliar Values
 Sky.pi2 = Math.PI * 2;
+Sky.pid2 = Math.PI / 2;
 
 //Function Prototype
 Sky.prototype = Object.create(THREE.Scene.prototype);
@@ -88,47 +89,69 @@ function update()
 		//Time in % of day
 		var time = (this.time / this.day_time);
 
-		//Update sky color
-
-		//6h -> 12h
+		//0H - 6H (night)
 		if(time < 0.25)
 		{
-			var t = time * 4;
-			var f = 1 - t;
-
-			this.sky.material.uniforms.top_color.value.setRGB(f*Sky.color_top[0].r + t*Sky.color_top[1].r, f*Sky.color_top[0].g + t*Sky.color_top[1].g, f*Sky.color_top[0].b + t*Sky.color_top[1].b);
-			this.sky.material.uniforms.bottom_color.value.setRGB(f*Sky.color_bottom[0].r + t*Sky.color_bottom[1].r, f*Sky.color_bottom[0].g + t*Sky.color_bottom[1].g, f*Sky.color_bottom[0].b + t*Sky.color_bottom[1].b);
+			this.sky.material.uniforms.top_color.value.setRGB(Sky.color_top[3].r, Sky.color_top[3].g, Sky.color_top[3].b);
+			this.sky.material.uniforms.bottom_color.value.setRGB(Sky.color_bottom[3].r, Sky.color_bottom[3].g, Sky.color_bottom[3].b);
 		}
-		//12h -> 18h
-		else if(time < 0.5)
+		//6H - 7H (night to morning)
+		else if(time < 0.292)
 		{
-			var t = (time-0.25) * 4;
-			var f = 1 - t;
-
-			this.sky.material.uniforms.top_color.value.setRGB(f*Sky.color_top[1].r + t*Sky.color_top[2].r, f*Sky.color_top[1].g + t*Sky.color_top[2].g, f*Sky.color_top[1].b + t*Sky.color_top[2].b);
-			this.sky.material.uniforms.bottom_color.value.setRGB(f*Sky.color_bottom[1].r + t*Sky.color_bottom[2].r, f*Sky.color_bottom[1].g + t*Sky.color_bottom[2].g, f*Sky.color_bottom[1].b + t*Sky.color_bottom[2].b);
-		}
-		//18H -> 24h
-		else if(time < 0.75)
-		{
-			var t = (time-0.5) * 4;
-			var f = 1 - t;
-
-			this.sky.material.uniforms.top_color.value.setRGB(f*Sky.color_top[2].r + t*Sky.color_top[3].r, f*Sky.color_top[2].g + t*Sky.color_top[3].g, f*Sky.color_top[2].b + t*Sky.color_top[3].b);
-			this.sky.material.uniforms.bottom_color.value.setRGB(f*Sky.color_bottom[2].r + t*Sky.color_bottom[3].r, f*Sky.color_bottom[2].g + t*Sky.color_bottom[3].g, f*Sky.color_bottom[2].b + t*Sky.color_bottom[3].b);
-		}
-		//0H -> 6h
-		else
-		{
-			var t = (time-0.75) * 4;
+			var t = (time-0.25) * 23.81;
 			var f = 1 - t;
 
 			this.sky.material.uniforms.top_color.value.setRGB(f*Sky.color_top[3].r + t*Sky.color_top[0].r, f*Sky.color_top[3].g + t*Sky.color_top[0].g, f*Sky.color_top[3].b + t*Sky.color_top[0].b);
 			this.sky.material.uniforms.bottom_color.value.setRGB(f*Sky.color_bottom[3].r + t*Sky.color_bottom[0].r, f*Sky.color_bottom[3].g + t*Sky.color_bottom[0].g, f*Sky.color_bottom[3].b + t*Sky.color_bottom[0].b);
 		}
+		//7H - 10H (morning)
+		else if(time < 0.4167)
+		{
+			this.sky.material.uniforms.top_color.value.setRGB(Sky.color_top[0].r, Sky.color_top[0].g, Sky.color_top[0].b);
+			this.sky.material.uniforms.bottom_color.value.setRGB(Sky.color_bottom[0].r, Sky.color_bottom[0].g, Sky.color_bottom[0].b);
+		}
+		//10H - 12H (morning to noon)
+		else if(time < 0.5)
+		{
+			var t = (time-0.4167) * 12;
+			var f = 1 - t;
+
+			this.sky.material.uniforms.top_color.value.setRGB(f*Sky.color_top[0].r + t*Sky.color_top[1].r, f*Sky.color_top[0].g + t*Sky.color_top[1].g, f*Sky.color_top[0].b + t*Sky.color_top[1].b);
+			this.sky.material.uniforms.bottom_color.value.setRGB(f*Sky.color_bottom[0].r + t*Sky.color_bottom[1].r, f*Sky.color_bottom[0].g + t*Sky.color_bottom[1].g, f*Sky.color_bottom[0].b + t*Sky.color_bottom[1].b);
+		}
+		//12H - 17H (noon)
+		else if(time < 0.708)
+		{
+			this.sky.material.uniforms.top_color.value.setRGB(Sky.color_top[1].r, Sky.color_top[1].g, Sky.color_top[1].b);
+			this.sky.material.uniforms.bottom_color.value.setRGB(Sky.color_bottom[1].r, Sky.color_bottom[1].g, Sky.color_bottom[1].b);
+		}
+		//17H -> 19h (noon to afternoon)
+		else if(time < 0.792)
+		{
+			var t = (time-0.708) * 11.90476;
+			var f = 1 - t;
+
+			this.sky.material.uniforms.top_color.value.setRGB(f*Sky.color_top[1].r + t*Sky.color_top[2].r, f*Sky.color_top[1].g + t*Sky.color_top[2].g, f*Sky.color_top[1].b + t*Sky.color_top[2].b);
+			this.sky.material.uniforms.bottom_color.value.setRGB(f*Sky.color_bottom[1].r + t*Sky.color_bottom[2].r, f*Sky.color_bottom[1].g + t*Sky.color_bottom[2].g, f*Sky.color_bottom[1].b + t*Sky.color_bottom[2].b);
+		}
+		//19H -> 21H (afternoon to night)
+		else if(time < 0.875)
+		{
+			var t = (time-0.792) * 12.048;
+			var f = 1 - t;
+
+			this.sky.material.uniforms.top_color.value.setRGB(f*Sky.color_top[2].r + t*Sky.color_top[3].r, f*Sky.color_top[2].g + t*Sky.color_top[3].g, f*Sky.color_top[2].b + t*Sky.color_top[3].b);
+			this.sky.material.uniforms.bottom_color.value.setRGB(f*Sky.color_bottom[2].r + t*Sky.color_bottom[3].r, f*Sky.color_bottom[2].g + t*Sky.color_bottom[3].g, f*Sky.color_bottom[2].b + t*Sky.color_bottom[3].b);
+		}
+		//21H -> 24H (night)
+		else
+		{
+			this.sky.material.uniforms.top_color.value.setRGB(Sky.color_top[3].r, Sky.color_top[3].g, Sky.color_top[3].b);
+			this.sky.material.uniforms.bottom_color.value.setRGB(Sky.color_bottom[3].r, Sky.color_bottom[3].g, Sky.color_bottom[3].b);
+		}
 
 		//Update sun position
-		var rotation = Sky.pi2 * time;
+		var rotation = (Sky.pi2 * time) - Sky.pid2;
 		this.sun.position.x = this.distance * Math.cos(rotation);
 		this.sun.position.y = this.distance * Math.sin(rotation);
 	}
