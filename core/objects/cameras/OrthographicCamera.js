e822bc9a8108f2536bcf4a3db705a857d4331a1f
José Ferrão <joseferrao@ua.pt>
2016-04-24 03:22:41 +0100

    Program and scene initialization
    Camera attaching working
    Changes in cameras

--- core/objects/cameras/OrthographicCamera.js
+++ core/objects/cameras/OrthographicCamera.js
@@ -1,26 +1,36 @@
-function OrthographicCamera(left, right, top, bottom, near, far)
+function OrthographicCamera(left, right, top, bottom, near, far)//(size, aspect, near, far, mode)
 {
+	/*this.size = 5;
+	this.aspect = 1;
+	this.mode = OrthographicCamera.FIXED_VERTICAL;
+
+	if(mode !== undefined)
+	{
+		this.mode = mode;
+	}*/
+
 	THREE.OrthographicCamera.call(this, left, right, top, bottom, near, far);
 
 	this.name = "orthographic_camera";
-
-	this.rotationAutoUpdate = true;
 }
 
 //Function Prototype
 OrthographicCamera.prototype = Object.create(THREE.OrthographicCamera.prototype);
 OrthographicCamera.prototype.icon = "editor/files/icons/camera/orthographic.png";
 
-//Runtime functions
 OrthographicCamera.prototype.update = update;
 OrthographicCamera.prototype.initialize = initialize;
+
+//Camera scale mode
+OrthographicCamera.FIXED_VERTICAL = 0;
+OrthographicCamera.FIXED_HORIZONTAL = 1;
 
 //Initialize
 function initialize()
 {
 	for(var i = 0; i < this.children.length; i++)
 	{
-		if(this.children[i].initialize != undefined)
+		if(this.children[i].initialize !== undefined)
 		{
 			this.children[i].initialize();
 		}
@@ -32,7 +42,7 @@ function update()
 {
 	for(var i = 0; i < this.children.length; i++)
 	{
-		if(this.children[i].update != undefined)
+		if(this.children[i].update !== undefined)
 		{
 			this.children[i].update();
 		}
