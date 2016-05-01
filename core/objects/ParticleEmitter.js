466be5e12c8204a247f34238a541d7a9e6b50292
José Ferrão <joseferrao@ua.pt>
2016-05-02 00:02:39 +0100

    derp

--- core/objects/ParticleEmitter.js
+++ core/objects/ParticleEmitter.js
@@ -11,15 +11,20 @@ function ParticleEmitter()
 	{
 		texture:
 		{
-		value: THREE.ImageUtils.loadTexture('./data/particle.png')
+			value: THREE.ImageUtils.loadTexture("./data/particle.png")
 		}
 	});
 
 	this.emitter = new SPE.Emitter(
-	{
+	{		
+		particleCount: 200000,
+		direction: 1,
+		duration: null, //seconds
+
 		maxAge:
 		{
-			value: 3
+			value: 2,
+			spread: 0
 		},
 
 		position:
@@ -28,29 +33,47 @@ function ParticleEmitter()
 			spread: new THREE.Vector3(0, 0, 0)
 		},
 
-		acceleration:
-		{
-			value: new THREE.Vector3(0, -10, 0),
-			spread: new THREE.Vector3( 10, 0, 10 )
-		},
-
 		velocity:
 		{
 			value: new THREE.Vector3(0, 25, 0),
 			spread: new THREE.Vector3(10, 7.5, 10)
 		},
 
+		acceleration:
+		{
+			value: new THREE.Vector3(0, -10, 0),
+			spread: new THREE.Vector3(10, 0, 10)
+		},
+
+		wiggle:
+		{
+			value: 0,
+			spread: 0
+		},
+
 		color:
 		{
-			value: [ new THREE.Color('white'), new THREE.Color('red') ]
+			value: [new THREE.Color(1, 1, 1), new THREE.Color(1, 0, 0)],
+			spread: [new THREE.Color(0, 0, 0), new THREE.Color(0.1, 0.1, 0.1)]
+		},
+
+		opacity:
+		{
+			value: 1,
+			spread: 0
 		},
 
 		size:
 		{
-			value: 1
+			value: 1,
+			spread: 0
 		},
 
-		particleCount: 20000
+		angle:
+		{
+			value: 0,
+			spread: 0
+		}
 	});
 
 	this.group.addEmitter(this.emitter);
