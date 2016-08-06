bd0be32bc40cd1900cc640ccfaa80e8f9752fab8
Tentone <tentone@outlook.com>
2016-08-06 20:57:42 +0100

    Fixed physics shape serialization

--- core/ObjectLoader.js
+++ core/ObjectLoader.js
@@ -474,24 +474,25 @@ function parseObject(data, geometries, materials, textures)
 			object.body.collisionFilterGroup = data.body.collisionFilterGroup;
 			object.body.collisionFilterMask = data.body.collisionFilterMask;
 			object.body.fixedRotation = data.body.fixedRotation;
-			var shapes = object.body.shapes;
+
+			var shapes = data.body.shapes;
 			for(var i = 0; i < shapes.length; i++)
 			{
 				var shape = shapes[i];
+
 				if(shape.type === CANNON.Shape.types.SPHERE)
 				{
-					//TODO <ADD CODE HERE>
+					object.body.addShape(new CANNON.Sphere(shape.radius));
 				}
 				else if(shape.type === CANNON.Shape.types.BOX)
 				{
-					//object.body.addShape(new CANNON.Box(new CANNON.Vec3(0.5, 0.5, 0.5)));
+					object.body.addShape(new CANNON.Box(new CANNON.Vec3(shape.halfExtents.x, shape.halfExtents.y, shape.halfExtents.z)));
 				}
 				else if(shape.type === CANNON.Shape.types.PARTICLE)
 				{
 					object.body.addShape(new CANNON.Particle());
 				}
 			}
-
 			break;
 
 		case "ParticleEmiter":
