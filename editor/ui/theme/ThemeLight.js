06be9cd312fea801161615880077bfa5caf7a8cf
Tentone <tentone@outlook.com>
2016-08-13 18:19:04 +0100

    Added WebVR polyfill for carboard support

--- editor/ui/theme/ThemeLight.js
+++ editor/ui/theme/ThemeLight.js
@@ -11,8 +11,8 @@ function ThemeLight()
 	this.button_over_color = "#CCCCCC";
 	this.button_light_color = "#999999";
 	this.box_color = "#DDDDDD";
-	this.text_color = "#000000";
-	this.icon_color = "#000000";
+	this.text_color = "#FFFFFF";
+	this.icon_color = "#FFFFFF";
 
 	document.body.style.color = this.text_color;
 	document.body.style.fontFamily = "Arial";
