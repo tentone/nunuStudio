88dabe1dc444b140fb140633c786dc6337cae746
Tentone <tentone@outlook.com>
2016-10-10 13:23:09 +0100

    Fixed Text3D  bug and added get resource by name methods
    Updated threejs

--- source/core/objects/mesh/Text3D.js
+++ source/core/objects/mesh/Text3D.js
@@ -2,7 +2,7 @@
 
 function Text3D(text, material, font)
 {
-	THREE.Mesh.call(this, new THREE.TextGeometry(text, {font: font}), material);
+	THREE.Mesh.call(this, undefined, material);
 	
 	this.name = "text";
 	this.type = "Text3D";
@@ -10,12 +10,13 @@ function Text3D(text, material, font)
 	this.scale.set(0.02, 0.02, 0.02);
 
 	this.font = font;
+
 	this.text = text;
-			
 	this.height = 50;
 	this.bevel = false;
 	this.bevel_thickness = 10;
 	this.bevel_size = 8;
+	this.setText("text");
 
 	this.receiveShadow = true;
 	this.castShadow = true;
@@ -27,8 +28,12 @@ Text3D.prototype = Object.create(THREE.Mesh.prototype);
 Text3D.prototype.setText = function(text)
 {
 	this.text = text;
-	this.geometry.dispose();
 
+	if(this.geometry !== undefined)
+	{
+		this.geometry.dispose();
+	}
+	
 	var options = 
 	{
 		font: this.font,
@@ -48,7 +53,11 @@ Text3D.prototype.dispose = function()
 	{
 		this.material.dispose();
 	}
-	this.geometry.dispose();
+	
+	if(this.geometry !== undefined)
+	{
+		this.geometry.dispose();
+	}
 
 	for(var i = 0; i < this.children.length; i++)
 	{
