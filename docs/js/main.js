73a2ad5f3bbe2c849d83839b6d81e6783173acc9
Tentone <tentone@outlook.com>
2017-02-06 15:31:51 +0000

    Webpage updated

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,45 +1,22 @@
-$(document).ready(function(){
-	
-	
-	//------------------------------------//
-	//Navbar//
-	//------------------------------------//
-			var menu = $('.navbar');
-			$(window).bind('scroll', function(e){
-				if($(window).scrollTop() > 140){
-					if(!menu.hasClass('open')){
-						menu.addClass('open');
-					}
-				}else{
-					if(menu.hasClass('open')){
-						menu.removeClass('open');
-					}
-				}
-			});
-	
-	
-	//------------------------------------//
-	//Scroll To//
-	//------------------------------------//
+$(document).ready(function()
+{
+	var menu = $('.navbar');
+	$(window).bind('scroll', function(e){
+		if($(window).scrollTop() > 140){
+			if(!menu.hasClass('open')){
+				menu.addClass('open');
+			}
+		}else{
+			if(menu.hasClass('open')){
+				menu.removeClass('open');
+			}
+		}
+	});
+
+
 	$(".scroll").click(function(event){		
 		event.preventDefault();
 		$('html,body').animate({scrollTop:$(this.hash).offset().top}, 800);
 		
 	});
-	
-	//------------------------------------//
-	//Wow Animation//
-	//------------------------------------// 
-	wow = new WOW(
-				{
-					boxClass:     'wow',      // animated element css class (default is wow)
-					animateClass: 'animated', // animation css class (default is animated)
-					offset:       0,          // distance to the element when triggering the animation (default is 0)
-					mobile:       false        // trigger animations on mobile devices (true is default)
-				}
-			);
-			wow.init();
-
-
-	
 });
