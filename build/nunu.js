5102558df32045ff16ac29579308691372233115
Tentone <tentone@outlook.com>
2017-02-17 03:31:36 +0000

    Optimizing tree views

--- build/nunu.js
+++ build/nunu.js
@@ -30,7 +30,7 @@ function Nunu() {
 }
 Nunu.NAME = "nunuStudio";
 Nunu.VERSION = "V0.8.9.20 Alpha";
-Nunu.TIMESTAMP = "201702170100";
+Nunu.TIMESTAMP = "201702170329";
 Nunu.webvrAvailable = function() {
   return void 0 !== navigator.getVRDisplays;
 };
