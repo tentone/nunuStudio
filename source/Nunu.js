cc7d2e898e5ecfac523b5b7000a0e7781a648bcb
Tentone <tentone@outlook.com>
2017-03-28 16:43:13 +0100

    hotFix

--- source/Nunu.js
+++ source/Nunu.js
@@ -42,11 +42,7 @@ Nunu.VERSION = "V0.8.9.23 Alpha";
  * @attribute TIMESTAMP
  * @type {String}
  */
-<<<<<<< Updated upstream
-Nunu.TIMESTAMP = "201703272237";
-=======
 Nunu.TIMESTAMP = "201703281634";
->>>>>>> Stashed changes
 
 /**
  * Check if host supports WebVR and if there is a VR display available.
