a2b9985d8388637d7d2d2d3590400808d891e74f
Tentone <tentone@outlook.com>
2017-05-07 11:40:23 +0100

    Fixed mobile input position

--- source/Nunu.js
+++ source/Nunu.js
@@ -36,7 +36,7 @@ Nunu.VERSION = "V0.8.9.26 Alpha";
  * @attribute TIMESTAMP
  * @type {String}
  */
-Nunu.TIMESTAMP = "201705061842";
+Nunu.TIMESTAMP = "201705071136";
 
 /**
  * Check if host supports WebVR and if there is a VR display available.
--- source/core/input/Keyboard.js
+++ source/core/input/Keyboard.js
@@ -66,8 +66,6 @@ Keyboard.prototype = Keyboard;
 /**
  * Update key flags synchronously.
  * 
- * Called automatically by the app runtime.
- * 
  * @method update
  */
 Keyboard.update = function()
@@ -142,8 +140,6 @@ Keyboard.keyJustReleased = function(key)
 /**
  * Dispose keyboard events.
  * 
- * Called automatically by the app runtime.
- * 
  * @method dispose
  */
 Keyboard.dispose = function()
@@ -161,78 +157,91 @@ Keyboard.dispose = function()
  * @type {Number}
  */
 Keyboard.TAB = 9;
+
 /**
  * ENTER key
  * @attribute ENTER
  * @type {Number}
  */
 Keyboard.ENTER = 13;
+
 /**
  * SHIFT key
  * @attribute SHIFT
  * @type {Number}
  */
 Keyboard.SHIFT = 16;
+
 /**
  * CTRL key
  * @attribute CTRL
  * @type {Number}
  */
 Keyboard.CTRL = 17;
+
 /**
  * ALT key
  * @attribute ALT
  * @type {Number}
  */
 Keyboard.ALT = 18;
+
 /**
  * CAPS_LOCK key
  * @attribute CAPS_LOCK
  * @type {Number}
  */
 Keyboard.CAPS_LOCK = 20;
+
 /**
  * ESC key
  * @attribute ESC
  * @type {Number}
  */
 Keyboard.ESC = 27;
+
 /**
  * SPACEBAR key
  * @attribute SPACEBAR
  * @type {Number}
  */
 Keyboard.SPACEBAR = 32;
+
 /**
  * PAGE_UP key
  * @attribute PAGE_UP
  * @type {Number}
  */
 Keyboard.PAGE_UP = 33;
+
 /**
  * PAGE_DOWN key
  * @attribute PAGE_DOWN
  * @type {Number}
  */
 Keyboard.PAGE_DOWN = 34;
+
 /**
  * END key
  * @attribute END
  * @type {Number}
  */
 Keyboard.END = 35;
+
 /**
  * HOME key
  * @attribute HOME
  * @type {Number}
  */
 Keyboard.HOME = 36;
+
 /**
  * INSERT key
  * @attribute INSERT
  * @type {Number}
  */
 Keyboard.INSERT = 45;
+
 /**
  * DEL key
  * @attribute DEL
@@ -246,18 +255,21 @@ Keyboard.DEL = 46;
  * @type {Number}
  */
 Keyboard.LEFT = 37;
+
 /**
  * RIGHT key
  * @attribute RIGHT
  * @type {Number}
  */
 Keyboard.RIGHT = 39;
+
 /**
  * UP key
  * @attribute UP
  * @type {Number}
  */
 Keyboard.UP = 38;
+
 /**
  * DOWN key
  * @attribute DOWN
@@ -271,54 +283,63 @@ Keyboard.DOWN = 40;
  * @type {Number}
  */
 Keyboard.NUM0 = 48;
+
 /**
  * NUM1 key
  * @attribute NUM1
  * @type {Number}
  */
 Keyboard.NUM1 = 49;
+
 /**
  * NUM2 key
  * @attribute NUM2
  * @type {Number}
  */
 Keyboard.NUM2 = 50;
+
 /**
  * NUM3 key
  * @attribute NUM3
  * @type {Number}
  */
 Keyboard.NUM3 = 51;
+
 /**
  * NUM4 key
  * @attribute NUM4
  * @type {Number}
  */
 Keyboard.NUM4 = 52;
+
 /**
  * NUM5 key
  * @attribute NUM5
  * @type {Number}
  */
 Keyboard.NUM5 = 53;
+
 /**
  * NUM6 key
  * @attribute NUM6
  * @type {Number}
  */
 Keyboard.NUM6 = 54;
+
 /**
  * NUM7 key
  * @attribute NUM7
  * @type {Number}
  */
 Keyboard.NUM7 = 55;
+
 /**
  * NUM8 key
  * @attribute NUM8
  * @type {Number}
  */
 Keyboard.NUM8 = 56;
+
 /**
  * NUM9 key
  * @attribute NUM9
@@ -332,150 +353,175 @@ Keyboard.NUM9 = 57;
  * @type {Number}
  */
 Keyboard.A = 65;
+
 /**
  * B key
  * @attribute B
  * @type {Number}
  */
 Keyboard.B = 66;
+
 /**
  * C key
  * @attribute C
  * @type {Number}
  */
 Keyboard.C = 67;
+
 /**
  * D key
  * @attribute D
  * @type {Number}
  */
 Keyboard.D = 68;
+
 /**
  * E key
  * @attribute E
  * @type {Number}
  */
 Keyboard.E = 69;
+
 /**
  * F key
  * @attribute F
  * @type {Number}
  */
 Keyboard.F = 70;
+
 /**
  * G key
  * @attribute G
  * @type {Number}
  */
 Keyboard.G = 71;
+
 /**
  * H key
  * @attribute H
  * @type {Number}
  */
 Keyboard.H = 72;
+
 /**
  * I key
  * @attribute I
  * @type {Number}
  */
 Keyboard.I = 73;
+
 /**
  * J key
  * @attribute J
  * @type {Number}
  */
 Keyboard.J = 74;
+
 /**
  * K key
  * @attribute K
  * @type {Number}
  */
 Keyboard.K = 75;
+
 /**
  * L key
  * @attribute L
  * @type {Number}
  */
 Keyboard.L = 76;
+
 /**
  * M key
  * @attribute M
  * @type {Number}
  */
 Keyboard.M = 77;
+
 /**
  * N key
  * @attribute N
  * @type {Number}
  */
 Keyboard.N = 78;
+
 /**
  * O key
  * @attribute O
  * @type {Number}
  */
 Keyboard.O = 79;
+
 /**
  * P key
  * @attribute P
  * @type {Number}
  */
 Keyboard.P = 80;
+
 /**
  * Q key
  * @attribute Q
  * @type {Number}
  */
 Keyboard.Q = 81;
+
 /**
  * R key
  * @attribute R
  * @type {Number}
  */
 Keyboard.R = 82;
+
 /**
  * S key
  * @attribute S
  * @type {Number}
  */
 Keyboard.S = 83;
+
 /**
  * T key
  * @attribute T
  * @type {Number}
  */
 Keyboard.T = 84;
+
 /**
  * U key
  * @attribute U
  * @type {Number}
  */
 Keyboard.U = 85;
+
 /**
  * V key
  * @attribute V
  * @type {Number}
  */
 Keyboard.V = 86;
+
 /**
  * W key
  * @attribute W
  * @type {Number}
  */
 Keyboard.W = 87;
+
 /**
  * X key
  * @attribute X
  * @type {Number}
  */
 Keyboard.X = 88;
+
 /**
  * Y key
  * @attribute Y
  * @type {Number}
  */
 Keyboard.Y = 89;
+
 /**
  * Z key
  * @attribute Z
@@ -483,72 +529,84 @@ Keyboard.Y = 89;
  */
 Keyboard.Z = 90;
 
+
 /**
  * F1 key
  * @attribute F1
  * @type {Number}
  */
 Keyboard.F1 = 112;
+
 /**
  * F2 key
  * @attribute F2
  * @type {Number}
  */
 Keyboard.F2 = 113;
+
 /**
  * F3 key
  * @attribute F3
  * @type {Number}
  */
 Keyboard.F3 = 114;
+
 /**
  * F4 key
  * @attribute F4
  * @type {Number}
  */
 Keyboard.F4 = 115;
+
 /**
  * F5 key
  * @attribute F5
  * @type {Number}
  */
 Keyboard.F5 = 116;
+
 /**
  * F6 key
  * @attribute F6
  * @type {Number}
  */
 Keyboard.F6 = 117;
+
 /**
  * F7 key
  * @attribute F7
  * @type {Number}
  */
 Keyboard.F7 = 118;
+
 /**
  * F8 key
  * @attribute F8
  * @type {Number}
  */
 Keyboard.F8 = 119;
+
 /**
  * F9 key
  * @attribute F9
  * @type {Number}
  */
 Keyboard.F9 = 120;
+
 /**
  * F10 key
  * @attribute F10
  * @type {Number}
  */
 Keyboard.F10 = 121;
+
 /**
  * F11 key
  * @attribute F11
  * @type {Number}
  */
 Keyboard.F11 = 122;
+
 /**
  * F12 key
  * @attribute F12
