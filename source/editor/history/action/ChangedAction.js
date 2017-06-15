5cf9c5c569277e056058cbb3b6d37671c921aa51
Tentone <tentone@outlook.com>
2017-06-15 16:33:14 +0100

    I have no idea what i have changed

--- source/editor/history/action/ChangedAction.js
+++ source/editor/history/action/ChangedAction.js
@@ -4,3 +4,18 @@ function ChangedAction()
 {
 	
 }
+
+ChangedAction.prototype.apply = function()
+{
+	//TODO <ADD CODE HERE>
+};
+
+ChangedAction.prototype.undo = function()
+{
+	//TODO <ADD CODE HERE>
+};
+
+ChangedAction.prototype.redo = function()
+{
+	//TODO <ADD CODE HERE>
+};
