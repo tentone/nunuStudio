eeb25cab7adc57959aeea67fe27c0ea3d8ef0a69
Tentone <tentone@outlook.com>
2017-06-15 16:33:14 +0100

    I have no idea what i have changed

---
+++ source/editor/history/action/Action.js
@@ -0,0 +1,24 @@
+"use strict";
+
+function Action(type, target)
+{
+	this.timestamp = 0;
+
+	this.type = -1;
+	this.target = null;
+}
+
+Action.prototype.apply = function()
+{
+	//TODO <ADD CODE HERE>
+};
+
+Action.prototype.redo = function()
+{
+	//TODO <ADD CODE HERE>
+};
+
+Action.prototype.undo = function()
+{
+	//TODO <ADD CODE HERE>
+};
