c3320d9d98dd115ee1ceaf5339f64e6db20ba8ef
Tentone <tentone@outlook.com>
2017-07-03 20:42:42 +0100

    Fixed GLTF import

--- source/core/objects/Scene.js
+++ source/core/objects/Scene.js
@@ -224,7 +224,7 @@ Scene.prototype.setFogMode = function(mode)
  */
 Scene.prototype.toJSON = function(meta)
 {
-	if(this.parent !== null && this.parent.type !== "Program")
+	if(this.parent.type !== "Program")
 	{
 		this.type = "Group";
 		console.warn("nunuStudio: Scene is not on top level serializing as Group.");
