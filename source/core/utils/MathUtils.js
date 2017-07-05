7606578066803d0cff8efe93750d07005578eb1e
Tentone <tentone@outlook.com>
2017-07-05 22:55:07 +0100

    Load dummy data for images when texture load fails

--- source/core/utils/MathUtils.js
+++ source/core/utils/MathUtils.js
@@ -32,3 +32,24 @@ MathUtils.pid3 = Math.PI / 3;
  * @type {Number}
  */
 MathUtils.pid2 = Math.PI / 2;
+
+/**
+ * Generates a random color code.
+ *
+ * Uses the #RRGGBB format.
+ *
+ * @method randomColor
+ * @return {String} Generated color code.
+ */
+MathUtils.randomColor = function()
+{
+	var letters = "0123456789ABCDEF";
+	var color = "#";
+
+	for(var i = 0; i < 6; i++)
+	{
+		color += letters[Math.floor(Math.random() * 16)];
+	}
+
+	return color;
+};
