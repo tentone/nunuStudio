46b97fb9d11ef35c9c3556bad218dd951fed8304
Tentone <tentone@outlook.com>
2017-07-08 19:11:24 +0100

    Updated threejs

---
+++ source/core/three/BufferGeometry.js
@@ -0,0 +1,110 @@
+"use strict";
+
+THREE.BufferGeometry.prototype.toJSON = function()
+{
+	var data =
+	{
+		metadata:
+		{
+			version: 4.5,
+			type: "BufferGeometry",
+			generator: "BufferGeometry.toJSON"
+		}
+	};
+	
+	data.uuid = this.uuid;
+	data.type = this.type;
+	data.name = this.name;
+
+	if(this.parameters !== undefined)
+	{
+		var parameters = this.parameters;
+
+		for(var key in parameters)
+		{
+			if(parameters[key] !== undefined)
+			{
+				data[key] = parameters[key];
+			}
+		}
+
+		return data;
+	}
+
+	data.data = {attributes: {}, morphAttributes: {}};
+
+	var index = this.index;
+
+	if(index !== null)
+	{
+		var array = Array.prototype.slice.call(index.array);
+
+		data.data.index =
+		{
+			type: index.array.constructor.name,
+			array: array
+		};
+	}
+
+	var attributes = this.attributes;
+
+	for(var key in attributes)
+	{
+
+		var attribute = attributes[key];
+
+		var array = Array.prototype.slice.call(attribute.array);
+
+		data.data.attributes[key] =
+		{
+			itemSize: attribute.itemSize,
+			type: attribute.array.constructor.name,
+			array: array,
+			normalized: attribute.normalized
+		};
+	}
+
+	var morphAttributes = this.morphAttributes;
+
+	for(var key in morphAttributes)
+	{
+		var attributeArray = this.morphAttributes[key];
+
+		var array = [];
+
+		for(var i = 0, il = attributeArray.length; i < il; i ++)
+		{
+			var attribute = attributeArray[i];
+
+			array.push({
+				name: attribute.name,
+				itemSize: attribute.itemSize,
+				type: attribute.array.constructor.name,
+				array: Array.prototype.slice.call(attribute.array),
+				normalized: attribute.normalized
+			});
+		}
+
+		data.data.morphAttributes[key] = array;
+	}
+
+	var groups = this.groups;
+
+	if(groups.length > 0)
+	{
+		data.data.groups = JSON.parse(JSON.stringify(groups));
+	}
+
+	var boundingSphere = this.boundingSphere;
+
+	if(boundingSphere !== null)
+	{
+		data.data.boundingSphere =
+		{
+			center: boundingSphere.center.toArray(),
+			radius: boundingSphere.radius
+		};
+	}
+
+	return data;
+};
---
+++ source/core/three/BufferGeometryLoader.js
@@ -0,0 +1,84 @@
+"use strict";
+
+THREE.BufferGeometryLoader.prototype.parse = function(json)
+{
+	var TYPED_ARRAYS =
+	{
+		Int8Array: Int8Array,
+		Uint8Array: Uint8Array,
+		Uint8ClampedArray: Uint8ClampedArray,
+		Int16Array: Int16Array,
+		Uint16Array: Uint16Array,
+		Int32Array: Int32Array,
+		Uint32Array: Uint32Array,
+		Float32Array: Float32Array,
+		Float64Array: Float64Array
+	};
+	var geometry = new THREE.BufferGeometry();
+	var index = json.data.index;
+
+	if(index !== undefined)
+	{
+		var typedArray = new TYPED_ARRAYS[index.type](index.array);
+		geometry.setIndex(new THREE.BufferAttribute(typedArray, 1));
+	}
+
+	var attributes = json.data.attributes;
+
+	for(var key in attributes)
+	{
+		var attribute = attributes[key];
+		var typedArray = new TYPED_ARRAYS[attribute.type](attribute.array);
+		geometry.addAttribute(key, new THREE.BufferAttribute(typedArray, attribute.itemSize, attribute.normalized));
+	}
+
+	var morphAttributes = json.data.morphAttributes;
+
+	for(var key in morphAttributes)
+	{
+		var attributeArray = morphAttributes[key];
+		var array = [];
+
+		for(var i = 0, il = attributeArray.length; i < il; i ++)
+		{
+			var attribute = attributeArray[i];
+			var typedArray = new TYPED_ARRAYS[attribute.type](attribute.array);
+
+			var bufferAttribute = new THREE.BufferAttribute(typedArray, attribute.itemSize, attribute.normalized);
+			if(attribute.name !== undefined)
+			{
+				bufferAttribute.name = attribute.name;
+			}
+			array.push(bufferAttribute);
+		}
+
+		geometry.morphAttributes[key] = array;
+	}
+
+	var groups = json.data.groups || json.data.drawcalls || json.data.offsets;
+
+	if(groups !== undefined)
+	{
+		for(var i = 0, n = groups.length; i !== n; ++i)
+		{
+			var group = groups[i];
+			geometry.addGroup(group.start, group.count, group.materialIndex);
+		}
+	}
+
+	var boundingSphere = json.data.boundingSphere;
+
+	if(boundingSphere !== undefined)
+	{
+		var center = new THREE.Vector3();
+
+		if(boundingSphere.center !== undefined)
+		{
+			center.fromArray(boundingSphere.center);
+		}
+
+		geometry.boundingSphere = new THREE.Sphere(center, boundingSphere.radius);
+	}
+
+	return geometry;
+};
