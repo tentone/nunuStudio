0a9bb79d675532ced4c064a4b2b646a42f6dc2a3
Tentone <tentone@outlook.com>
2017-09-21 23:40:21 +0100

    DOM Utils vertical horizontal visibility

--- source/editor/utils/DOMUtils.js
+++ source/editor/utils/DOMUtils.js
@@ -19,3 +19,33 @@ DOMUtils.isVisible = function(element)
 
 	return top >= window.pageYOffset && left >= window.pageXOffset && (top + height) <= (window.pageYOffset + window.innerHeight) && (left + width) <= (window.pageXOffset + window.innerWidth);
 };
+
+//Check if a DOM element is visible on X axis
+DOMUtils.isVisibleX = function(element)
+{
+	var top = element.offsetTop;
+	var height = element.offsetHeight;
+
+	while(element.offsetParent)
+	{
+		element = element.offsetParent;
+		top += element.offsetTop;
+	}
+
+	return top >= window.pageYOffset && (top + height) <= (window.pageYOffset + window.innerHeight)
+};
+
+//Check if a DOM element is visible on Y axis
+DOMUtils.isVisibleY = function(element)
+{
+	var left = element.offsetLeft;
+	var width = element.offsetWidth;
+
+	while(element.offsetParent)
+	{
+		element = element.offsetParent;
+		left += element.offsetLeft;
+	}
+
+	return left >= window.pageXOffset && (left + width) <= (window.pageXOffset + window.innerWidth);
+};
