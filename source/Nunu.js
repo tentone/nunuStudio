2edc624a67abe866e228564038afac703d5b2f24
Tentone <tentone@outlook.com>
2017-09-22 23:19:44 +0100

    Cube camera preview implemented

--- source/Nunu.js
+++ source/Nunu.js
@@ -36,7 +36,7 @@ Nunu.VERSION = "V0.9.2 Beta";
  * @attribute TIMESTAMP
  * @type {String}
  */
-Nunu.TIMESTAMP = "201709201318";
+Nunu.TIMESTAMP = "201709222318";
 
 /**
  * Check if host supports WebVR and if there is a VR display available.
