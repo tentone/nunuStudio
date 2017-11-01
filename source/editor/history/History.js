df9ff70b06961e110620b49511c86dfda8fbf572
Tentone <tentone@outlook.com>
2017-11-01 17:29:06 +0000

    Updated history to support redo and undo properly

--- source/editor/history/History.js
+++ source/editor/history/History.js
@@ -4,30 +4,45 @@
 function History(program)
 {
 	this.actions = [];
+	this.counter = 0;
 
-	this.position = 0;
+	this.position = -1;
 	this.limit = 10;
 }
 
 //Add change to program history
 History.prototype.add = function(action)
 {	
-	this.actions.push(action);
-	action.apply();
-
-	if(this.actions.length > this.limit)
+	//If there are more recent changes remove them
+	while(this.actions.length > this.position + 1)
 	{
 		this.actions.pop();
+	}
+
+	//Add action to list
+	this.actions.push(action);
+	this.position++;
+			
+	//Apply action
+	action.apply();
+	action.id = this.counter;
+	this.counter++;
+
+	//Limit actions size
+	while(this.actions.length > this.limit)
+	{
+		this.actions.splice(0, 1)
+		this.position = this.actions.length - 1;
 	}
 };
 
 //Revert last action
 History.prototype.undo = function()
 {
-	if(this.actions.length > 0)
+	if(this.actions.length > 0 && this.position >= 0)
 	{
-		var action = this.actions.pop();
-		action.revert();
+		this.actions[this.position].revert();
+		this.position--;
 		return true;
 	}
 
@@ -37,5 +52,12 @@ History.prototype.undo = function()
 //Redo last reverted action
 History.prototype.redo = function()
 {
+	if(this.position < this.actions.length - 1)
+	{
+		this.position++;
+		this.actions[this.position].apply();
+		return true;
+	}
+
 	return false;
 };
