655b862156a975db369eb5c015b23806cf95ec11
Tentone <tentone@outlook.com>
2017-11-19 17:49:15 +0000

    Clean pass node

--- source/editor/ui/tab/camera/postprocessing/PassNode.js
+++ source/editor/ui/tab/camera/postprocessing/PassNode.js
@@ -14,7 +14,7 @@ function PassNode(parent, name)
 	this.composer = null;
 
 	//Render pass
-	this.addText(name);
+	this.addText(name !== undefined ? name : "Pass Node");
 	this.nextRow();
 
 	var self = this;
@@ -40,33 +40,37 @@ function PassNode(parent, name)
 	});
 	this.add(this.renderToScreen);
 	this.nextRow();
-}
 
-PassNode.prototype = Object.create(Form.prototype);
-
-PassNode.prototype.addButtons = function()
-{
 	//Up
 	this.up = new Button(this.element);
 	this.up.size.set(50, 18);
 	this.up.setText("Up");
-	this.add(this.up);
+	this.up.setCallback(function()
+	{
+		//TODO <UP>
+	});
 
 	//Down
-	this.up = new Button(this.element);
-	this.up.size.set(50, 18);
-	this.up.setText("Down");
-	this.add(this.up);
+	this.down = new Button(this.element);
+	this.down.size.set(50, 18);
+	this.down.setText("Down");
+	this.down.setCallback(function()
+	{
+		//TODO <UP>
+	});
 
 	//Delete
-	this.up = new Button(this.element);
-	this.up.size.set(70, 18);
-	this.up.setText("Delete");
-	this.add(this.up);
-	this.nextRow();
+	this.delete = new Button(this.element);
+	this.delete.size.set(70, 18);
+	this.delete.setText("Delete");
+	this.down.setCallback(function()
+	{
+		//TODO <DELETE>
+		//self.composer
+	});
+}
 
-	this.updateInterface();
-};
+PassNode.prototype = Object.create(Form.prototype);
 
 PassNode.prototype.setPass = function(pass)
 {
@@ -78,5 +82,9 @@ PassNode.prototype.setPass = function(pass)
 
 PassNode.prototype.setComposer = function(composer)
 {
+	this.add(this.up);
+	this.add(this.down);
+	this.add(this.delete);
+
 	this.composer = composer;
 };
