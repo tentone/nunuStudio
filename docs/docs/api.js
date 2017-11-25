94a381e8c230b451129022e200e4a811b82d4b87
Tentone <tentone@outlook.com>
2017-11-25 17:07:16 +0000

    Updated builds

--- docs/docs/api.js
+++ docs/docs/api.js
@@ -27,7 +27,6 @@ YUI.add("yuidoc-meta", function(Y) {
         "Fog",
         "Font",
         "FontLoader",
-        "GCodeDecoder",
         "GCodeLoader",
         "Gamepad",
         "GeometryLoader",
