8575463dfbbb30cfa606e83e099a948d07da2c61
Tentone <tentone@outlook.com>
2017-11-26 16:44:30 +0000

    File resource

--- source/core/resources/File.js
+++ source/core/resources/File.js
@@ -1,34 +1,34 @@
 "use strict";
 
 /**
- * Text resource can be used to store text data or code.
+ * File resource can be used to store data or code.
  *
- * Text data can be used to store information to be consumend by the application or runtime code that can be imported by scripts.
+ * File data can be used to store information to be consumend by the application or runtime code that can be imported by scripts.
  *
- * @class Text
+ * @class File
  * @constructor
  * @extends {Resource}
  * @module Resources
  */
-function Text()
+function File()
 {
-	Resource.call(this, "text", "Text");
+	Resource.call(this, "File", "File");
 
 	this.format = "string";
 	this.encoding = "txt";
 	this.data = "";
 }
 
-Text.prototype = Object.create(Resource.prototype);
+File.prototype = Object.create(Resource.prototype);
 
 /**
- * Serialize text resource data to json.
+ * Serialize File resource data to json.
  *
  * @method toJSON
  * @param {meta} meta
  * @return {Object} data
  */
-Text.prototype.toJSON = function(meta)
+File.prototype.toJSON = function(meta)
 {
 	var data = Resource.prototype.toJSON.call(this, meta);
 
