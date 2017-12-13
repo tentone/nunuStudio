8e2d7d10eb4ae99a9e7b148eb3765bb15061c7d3
Tentone <tentone@outlook.com>
2017-12-13 22:42:37 +0000

    Bar

--- source/editor/ui/tab/AnimationTab.js
+++ source/editor/ui/tab/AnimationTab.js
@@ -4,6 +4,37 @@ function AnimationTab(parent, closeable, container, index)
 {
 	TabElement.call(this, parent, closeable, container, index, "Animation", Editor.filePath + "icons/misc/animation.png");
 
+	//Bar
+	this.bar = document.createElement("div");
+	this.bar.style.backgroundColor = Editor.theme.barColor;
+	this.bar.style.overflow = "visible";
+	this.bar.style.position = "absolute";
+	this.bar.style.width = "100%";
+	this.bar.style.height = "20px";
+	this.element.appendChild(this.bar);
+
+	/*var mixer, clock;
+
+	function initialize()
+	{
+		var box = scene.getObjectByName("box");
+		
+		var position = new VectorKeyframeTrack(".position", [0, 1, 2], [0,0,0, 0,10,0, 0,0,0]);
+		var color = new ColorKeyframeTrack(".material.color", [0, 1, 3], [1,0,0, 0,1,0, 0,0,1]);
+		
+		var clip = new AnimationClip("Animation", 3, [position, color]);
+		
+		mixer = new AnimationMixer(box);
+		mixer.clipAction(clip).play();
+		
+		clock = new Clock();
+		clock.start();
+	}
+
+	function update()
+	{
+		mixer.update(clock.getDelta());
+	}*/
 }
 
 AnimationTab.prototype = Object.create(TabElement.prototype);
