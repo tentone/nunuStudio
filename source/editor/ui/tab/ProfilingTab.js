c717d1420a97327aaa3b6c195812786e84b4c377
Tentone <tentone@outlook.com>
2017-12-31 13:27:45 +0000

    Rendere data profilling tab

--- source/editor/ui/tab/ProfilingTab.js
+++ source/editor/ui/tab/ProfilingTab.js
@@ -6,10 +6,55 @@ function ProfilingTab(parent, closeable, container, index)
 
 	this.dual = new DualDivisionResizable(this.element);
 	this.dual.divA.style.backgroundColor = Editor.theme.barColor;
+	this.dual.divA.style.overflow = "auto";
 	this.dual.tabPosition = 0.2;
+
+	//Form
+	this.form = new Form(this.dual.divA);
+
+	this.form.addText("Renderer");
+	this.form.nextRow();
+
+	this.form.addText("Draw Calls");
+	this.calls = this.form.addText("");
+	this.form.nextRow();
+
+	this.form.addText("Faces");
+	this.faces = this.form.addText("");
+	this.form.nextRow();
+
+	this.form.addText("Frame");
+	this.frame = this.form.addText("123");
+	this.form.nextRow();
+
+	this.form.addText("Points");
+	this.points = this.form.addText("123");
+	this.form.nextRow();
+
+	this.form.addText("Vertices");
+	this.vertices = this.form.addText("123");
+	this.form.nextRow();
+
+	this.form.updateInterface();
 }
 
 ProfilingTab.prototype = Object.create(TabElement.prototype);
+
+ProfilingTab.prototype.update = function()
+{
+	var tab = Interface.tab.getActual();
+
+	if(tab.renderer !== undefined)
+	{
+		var info = tab.renderer.info;
+
+		this.calls.setText(info.render.calls);
+		this.faces.setText(info.render.faces);
+		this.frame.setText(info.render.frame);
+		this.points.setText(info.render.points);
+		this.vertices.setText(info.render.vertices);
+	}
+};
 
 ProfilingTab.prototype.updateInterface = function()
 {
