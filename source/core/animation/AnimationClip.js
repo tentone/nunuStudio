8b1bde1cf37937bc1c2323984c26b0a83b7cfeec
Tentone <tentone@outlook.com>
2018-01-04 23:36:38 +0000

    Animation Clip

---
+++ source/core/animation/AnimationClip.js
@@ -0,0 +1,16 @@
+"use strict";
+
+function AnimationClip()
+{
+	THREE.AnimationClip.call(this, root);
+
+}
+
+AnimationClip.prototype = Object.create(THREE.AnimationClip.prototype);
+
+AnimationClip.prototype.toJSON = function(clip)
+{
+	var data = THREE.AnimationClip.prototype.toJSON.call(this, clip);
+
+	return data;
+}; 
