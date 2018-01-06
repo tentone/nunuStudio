7d2919c8363e7d03ba2489131bebba37669a0921
Tentone <tentone@outlook.com>
2018-01-06 00:21:43 +0000

    Canvas set resolution and get context

--- source/editor/ui/element/Canvas.js
+++ source/editor/ui/element/Canvas.js
@@ -8,3 +8,14 @@ function Canvas(parent)
 }
 
 Canvas.prototype = Object.create(Element.prototype);
+
+Canvas.prototype.setResolution = function(width, height)
+{
+	this.element.width = width;
+	this.element.height = height;
+};
+
+Canvas.prototype.getContext2D = function()
+{
+	return this.element.getContext("2d");
+};
