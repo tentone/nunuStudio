e62a365ad8d709191ba8af4f054c4c94ec199396
Tentone <tentone@outlook.com>
2018-02-08 18:02:01 +0000

    Refactor camera orientation

--- source/editor/CameraOrientation.js
+++ source/editor/CameraOrientation.js
@@ -19,7 +19,9 @@ function CameraOrientation()
 	var plane = new THREE.PlaneBufferGeometry(1, 1);
 
 	//Cube faces
-	this.xPos = new THREE.Mesh(plane, new THREE.MeshBasicMaterial({map: new Texture(Editor.filePath + "camera/xPos.png")}));
+	var texture = new Texture(Editor.filePath + "camera/xPos.png");
+	texture.format = THREE.RGBFormat;
+	this.xPos = new THREE.Mesh(plane, new THREE.MeshBasicMaterial({map: texture}));
 	this.xPos.code = CameraOrientation.X_POS;
 	this.xPos.position.set(0.5, 0, 0);
 	this.xPos.rotation.set(0, Math.PI / 2, 0);
@@ -27,7 +29,9 @@ function CameraOrientation()
 	this.xPos.updateMatrix();
 	this.scene.add(this.xPos);
 
-	this.xNeg = new THREE.Mesh(plane, new THREE.MeshBasicMaterial({map: new Texture(Editor.filePath + "camera/xNeg.png")}));
+	var texture = new Texture(Editor.filePath + "camera/xNeg.png");
+	texture.format = THREE.RGBFormat;
+	this.xNeg = new THREE.Mesh(plane, new THREE.MeshBasicMaterial({map: texture}));
 	this.xNeg.code = CameraOrientation.X_NEG;
 	this.xNeg.position.set(-0.5, 0, 0);
 	this.xNeg.rotation.set(0, -Math.PI / 2, 0);
@@ -35,7 +39,9 @@ function CameraOrientation()
 	this.xNeg.updateMatrix();
 	this.scene.add(this.xNeg);
 
-	this.yPos = new THREE.Mesh(plane, new THREE.MeshBasicMaterial({map: new Texture(Editor.filePath + "camera/yPos.png")}));
+	var texture = new Texture(Editor.filePath + "camera/yPos.png");
+	texture.format = THREE.RGBFormat;
+	this.yPos = new THREE.Mesh(plane, new THREE.MeshBasicMaterial({map: texture}));
 	this.yPos.code = CameraOrientation.Y_POS;
 	this.yPos.position.set(0, 0.5, 0);
 	this.yPos.rotation.set(-Math.PI / 2, 0, 0);
@@ -43,7 +49,9 @@ function CameraOrientation()
 	this.yPos.updateMatrix();
 	this.scene.add(this.yPos);
 
-	this.yNeg = new THREE.Mesh(plane, new THREE.MeshBasicMaterial({map: new Texture(Editor.filePath + "camera/yNeg.png")}));
+	var texture = new Texture(Editor.filePath + "camera/yNeg.png");
+	texture.format = THREE.RGBFormat;
+	this.yNeg = new THREE.Mesh(plane, new THREE.MeshBasicMaterial({map: texture}));
 	this.yNeg.code = CameraOrientation.Y_NEG;
 	this.yNeg.position.set(0, -0.5, 0);
 	this.yNeg.rotation.set(Math.PI / 2, 0, 0);
@@ -51,14 +59,18 @@ function CameraOrientation()
 	this.yNeg.updateMatrix();
 	this.scene.add(this.yNeg);
 
-	this.zPos = new THREE.Mesh(plane, new THREE.MeshBasicMaterial({map: new Texture(Editor.filePath + "camera/zPos.png")}));
+	var texture = new Texture(Editor.filePath + "camera/zPos.png");
+	texture.format = THREE.RGBFormat;
+	this.zPos = new THREE.Mesh(plane, new THREE.MeshBasicMaterial({map: texture}));
 	this.zPos.code = CameraOrientation.Z_POS;
 	this.zPos.position.set(0, 0, 0.5);
 	this.zPos.matrixAutoUpdate = false;
 	this.zPos.updateMatrix();
 	this.scene.add(this.zPos);
 
-	this.zNeg = new THREE.Mesh(plane, new THREE.MeshBasicMaterial({map: new Texture(Editor.filePath + "camera/zNeg.png")}));
+	var texture = new Texture(Editor.filePath + "camera/zNeg.png");
+	texture.format = THREE.RGBFormat;
+	this.zNeg = new THREE.Mesh(plane, new THREE.MeshBasicMaterial({map: texture}));
 	this.zNeg.code = CameraOrientation.Z_NEG;
 	this.zNeg.position.set(0, 0, -0.5);
 	this.zNeg.rotation.set(0, Math.PI, 0);
