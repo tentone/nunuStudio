5ac222c8c2eeea2407de96aeca226a4784a67745
Tentone <tentone@outlook.com>
2018-05-30 08:53:51 +0100

    Updated DRACO

---
+++ source/core/geometry/CompressedGeometry.js
@@ -0,0 +1,86 @@
+"use strict";
+
+function CompressedGeometry()
+{
+	THREE.BufferGeometry.call(this);
+
+	this.type = "CompressedGeometry";
+}
+
+CompressedGeometry.prototype = Object.create(THREE.BufferGeometry.prototype);
+
+CompressedGeometry.prototype.toJSON = function()
+{
+	var data = {
+		metadata:
+		{
+			version: 4.5,
+			type: "CompressedGeometry",
+			generator: "CompressedGeometry.toJSON"
+		}
+	};
+
+	data.uuid = this.uuid;
+	data.type = this.type;
+	data.name = this.name;
+
+	if(this.parameters !== undefined)
+	{
+		var parameters = this.parameters;
+
+		for(var key in parameters)
+		{
+			if(parameters[key] !== undefined)
+			{
+				data[key] = parameters[key];
+			}
+		}
+
+		return data;
+	}
+
+	data.data = {attributes: {}};
+
+	var index = this.index;
+	if(index !== null)
+	{
+		var array = Array.prototype.slice.call(index.array);
+		data.data.index =
+		{
+			type: index.array.constructor.name,
+			array: array
+		};
+	}
+
+	var attributes = this.attributes;
+	for(var key in attributes)
+	{
+		var attribute = attributes[key];
+		var array = Array.prototype.slice.call(attribute.array);
+
+		data.data.attributes[key] = {
+			itemSize: attribute.itemSize,
+			type: attribute.array.constructor.name,
+			array: array,
+			normalized: attribute.normalized
+		};
+	}
+
+	var groups = this.groups;
+	if(groups.length > 0)
+	{
+		data.data.groups = JSON.parse(JSON.stringify(groups));
+	}
+
+	var boundingSphere = this.boundingSphere;
+	if(boundingSphere !== null)
+	{
+		data.data.boundingSphere =
+		{
+			center: boundingSphere.center.toArray(),
+			radius: boundingSphere.radius
+		};
+	}
+
+	return data;
+};
