eb1ac00f440dbce125d06e9b9ae6908c0de94ccf
Tentone <tentone@outlook.com>
2018-06-04 20:51:38 +0100

    Clean up

--- source/editor/gui/element/tabs/TabElement.js
+++ source/editor/gui/element/tabs/TabElement.js
@@ -166,20 +166,3 @@ TabElement.prototype.setName = function(text)
 	this.title = text;
 	this.button.setName(text);
 };
-
-//Update Interface
-TabElement.prototype.updateInterface = function()
-{
-	if(this.visible)
-	{
-		this.element.style.display = "block";
-		this.element.style.top = this.position.y + "px";
-		this.element.style.left = this.position.x + "px";
-		this.element.style.width = this.size.x + "px";
-		this.element.style.height = this.size.y + "px";
-	}
-	else
-	{
-		this.element.style.display = "none";
-	}
-};
