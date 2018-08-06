f22207ffb964ac6e75357a172adcf46944484e7b
Tentone <tentone@outlook.com>
2018-08-06 15:52:42 +0100

    Context menu cleanup

--- source/editor/gui/element/dropdown/ContextMenu.js
+++ source/editor/gui/element/dropdown/ContextMenu.js
@@ -11,8 +11,14 @@ function ContextMenu(parent)
 {
 	Element.call(this, parent);
 
+	var self = this;
+
 	this.element.style.overflow = "visible";
 	this.element.style.zIndex = "300";
+	this.element.onmouseleave = function()
+	{
+		self.destroy();
+	};
 
 	this.offset = new THREE.Vector2(20, 10);
 	
@@ -23,14 +29,6 @@ function ContextMenu(parent)
 	 * @type {Array}
 	 */
 	this.options = [];
-
-	var self = this;
-
-	//Mouse leave
-	this.element.onmouseleave = function()
-	{
-		self.destroy();
-	};
 }
 
 ContextMenu.prototype = Object.create(Element.prototype);
@@ -107,39 +105,43 @@ ContextMenu.prototype.addMenu = function(name)
 	return menu;
 };
 
-ContextMenu.prototype.updateInterface = function()
+/**
+ * Update all options in the menu.
+ * 
+ * @method updateOptions
+ */
+ContextMenu.prototype.updateOptions = function()
 {
-	if(this.visible)
+	for(var i = 0; i < this.options.length; i++)
 	{
-		//Options
-		for(var i = 0; i < this.options.length; i++)
-		{
-			this.options[i].visible = this.visible;
-			this.options[i].size.copy(this.size);
-			this.options[i].position.set(0, this.size.y * i);
-			this.options[i].updateInterface();
-		}
-	
-		//Element
-		this.element.style.visibility = "visible";
-		this.element.style.top = (this.position.y - this.offset.y) + "px";
-		this.element.style.left = (this.position.x - this.offset.x) + "px";
-		this.element.style.width = this.size.x + "px";
-		this.element.style.height = (this.size.y * this.options.length) + "px";
-
-		//Check if its inside window
-		var out = DOMUtils.checkBorder(this.element);
-		if(out.x !== 0)
-		{
-			this.element.style.left = (this.position.x + this.offset.x - this.size.x) + "px"; 
-		}
-		if(out.y !== 0)
-		{
-			this.element.style.top = (this.position.y - this.offset.y - out.y) + "px";
-		}
+		this.options[i].size.copy(this.size);
+		this.options[i].position.set(0, this.size.y * i);
+		this.options[i].updateInterface();
 	}
-	else
+};
+
+ContextMenu.prototype.updateSize = function()
+{
+	this.element.style.width = this.size.x + "px";
+	this.element.style.height = (this.size.y * this.options.length) + "px";
+
+	this.updateOptions();
+};
+
+
+ContextMenu.prototype.updatePosition = function()
+{
+	this.element.style.top = (this.position.y - this.offset.y) + "px";
+	this.element.style.left = (this.position.x - this.offset.x) + "px";
+
+	//Check if its inside window
+	var out = DOMUtils.checkBorder(this.element);
+	if(out.x !== 0)
 	{
-		this.element.style.visibility = "hidden";
+		this.element.style.left = (this.position.x + this.offset.x - this.size.x) + "px"; 
+	}
+	if(out.y !== 0)
+	{
+		this.element.style.top = (this.position.y - this.offset.y - out.y) + "px";
 	}
 };
