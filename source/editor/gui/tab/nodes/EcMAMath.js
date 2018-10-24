cf119329c629c966292f32b0339861dff94085d6
AETeam <nothersgta@gmail.com>
2018-10-24 17:27:56 -0500

    Adding more nodes

--- source/editor/gui/tab/nodes/EcMAMath.js
+++ source/editor/gui/tab/nodes/EcMAMath.js
@@ -2,4 +2,19 @@ function pi() {
 	return Math.PI;
 }
 
+function round(toRound) {
+	return Math.round(toRound);
+}
+
+function pow(x, y) {
+	return Math.pow(x,y);
+}
+
+function sqrt(x) {
+	return Math.sqrt(x);
+}
+
 LiteGraph.wrapFunctionAsNode("Math/PI", pi, null, "number");
+LiteGraph.wrapFunctionAsNode("Math/Round", round, ["number"], "number");
+LiteGraph.wrapFunctionAsNode("Math/Pow", pow, ["number", "number"], "number");
+LiteGraph.wrapFunctionAsNode("Math/Sqrt", sqrt, ["number"], "number");
