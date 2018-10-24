82edbfd0f3e360f389322cfedbc836025eac3524
AETeam <nothersgta@gmail.com>
2018-10-24 17:33:58 -0500

    Still adding nodes

--- source/editor/gui/tab/nodes/EcMAMath.js
+++ source/editor/gui/tab/nodes/EcMAMath.js
@@ -14,7 +14,27 @@ function sqrt(x) {
 	return Math.sqrt(x);
 }
 
+function abs(x) {
+	return Math.abs(x);
+}
+
+function ceil(x) {
+	return Math.ceil(x);
+}
+
+function floor(x) {
+	return Math.floor(x);
+}
+
+function sin(x) {
+	return Math.sin(x);
+}
+
 LiteGraph.wrapFunctionAsNode("Math/PI", pi, null, "number");
 LiteGraph.wrapFunctionAsNode("Math/Round", round, ["number"], "number");
 LiteGraph.wrapFunctionAsNode("Math/Pow", pow, ["number", "number"], "number");
 LiteGraph.wrapFunctionAsNode("Math/Sqrt", sqrt, ["number"], "number");
+LiteGraph.wrapFunctionAsNode("Math/Abs", abs, ["number"], "number");
+LiteGraph.wrapFunctionAsNode("Math/Ceil", ceil, ["number"], "number");
+LiteGraph.wrapFunctionAsNode("Math/Floor", floor, ["number"], "number");
+LiteGraph.wrapFunctionAsNode("Math/Sin", sin, ["number"], "number");
