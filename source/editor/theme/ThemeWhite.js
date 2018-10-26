a827ab4bb0065d01575fbecd4fdc2cf4384edaef
AETeam <nothersgta@gmail.com>
2018-10-26 18:41:34 -0500

    A white theme??

---
+++ source/editor/theme/ThemeWhite.js
@@ -0,0 +1,38 @@
+"use strict";
+
+function ThemeWhite()
+{
+	this.name = "white";
+	this.author = "nothersingame";
+	this.info = "";
+	this.version = "v1.0";
+
+	//Font
+	this.font = "Arial";
+
+	//Color
+	this.barColor = "#222222";
+	this.sepColor = "#292929";
+	this.panelColor = "#333333";
+	this.resizeTabColor = "#222222";
+	this.boxColor = "#444444";
+	this.textColor = "#FFFFFF";
+	this.iconColor = "#FFFFFF";
+
+	//Button
+	this.buttonColor = "#222222";
+	this.buttonOverColor = "#555555";
+	this.buttonLightColor = "#333333";
+	
+	//Audio player
+	this.audioTrack = "#222222";
+	this.audioScrubber = "#FFFFFF";
+	this.audioProgress = "#555555";
+
+	//Body
+	document.body.style.fontFamily = this.font;
+	document.body.style.color = this.textColor;
+	document.body.style.fontSize = "12px";
+}
+
+ThemeManager.register(ThemeWhite, "dark");
