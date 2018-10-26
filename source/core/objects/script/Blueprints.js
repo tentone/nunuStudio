c0f22da69585e57dad81394622b40f0db8a64dd1
AETeam <nothersgta@gmail.com>
2018-10-25 19:03:23 -0500

    Let's try to set the nodes to an object

--- source/core/objects/script/Blueprints.js
+++ source/core/objects/script/Blueprints.js
@@ -3,6 +3,14 @@ function VisualScript(nodes) {
 
 	this.type = "Script";
 	this.name = "Visual";
+
+	this.nodes = (nodes !== undefined) ? nodes : {};
 }
 
 VisualScript.prototype = Object.create(THREE.Group.prototype);
+
+VisualScript.prototype.setNodes = function(nodes) {
+	if (nodes !== undefined) {
+		this.nodes = nodes;
+	}
+}
