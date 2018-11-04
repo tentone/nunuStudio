9db684d9f029f1608263b84d2f9c7dda67b25c9d
Tentone <tentone@outlook.com>
2018-11-04 10:48:50 +0000

    Video docs

--- build/build/build.js
+++ build/build/build.js
@@ -139,7 +139,16 @@ function addTimestamp(keyword, code)
 
 function compressCSS(code)
 {
-	return code.replace(/(\/\*([\s\S]*?)\*\/)/gm, "").replace(new RegExp("\n\n", "g"), "\n");
+    code = code.replace(/\/\*(?:(?!\*\/)[\s\S])*\*\/|[\r\n\t]+/g, '');
+    // now all comments, newlines and tabs have been removed
+    code = code.replace(/ {2,}/g, ' ');
+    // now there are no more than single adjacent spaces left
+    // now unnecessary: code = code.replace(/(\s)+\./g, ' .');
+    code = code.replace(/ ([{:}]) /g, '$1');
+    code = code.replace(/([;,]) /g, '$1');
+    code = code.replace(/ !/g, '!');
+
+	return code;
 }
 
 function removeJSComments(code)
