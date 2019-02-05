d2e2847c4f7878ea3bb4be5903a0262cf73603be
Tentone <tentone@outlook.com>
2019-02-05 09:21:10 +0000

    Clean up

--- source/core/postprocessing/pass/UnrealBloomPass.js
+++ source/core/postprocessing/pass/UnrealBloomPass.js
@@ -341,7 +341,7 @@ UnrealBloomPass.getSeperableBlurMaterial = function(kernelRadius)
 			void main()\n\
 			{\n\
 				vUv = uv;\n\
-				gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );\n\
+				gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);\n\
 			}",
 		fragmentShader: "\n\
 			#include <common>\n\
@@ -352,7 +352,7 @@ UnrealBloomPass.getSeperableBlurMaterial = function(kernelRadius)
 			\n\
 			float gaussianPdf(in float x, in float sigma)\n\
 			{\n\
-				return 0.39894 * exp( -0.5 * x * x/( sigma * sigma))/sigma;\n\
+				return 0.39894 * exp(-0.5 * x * x / (sigma * sigma)) / sigma;\n\
 			}\n\
 			\n\
 			void main()\n\
@@ -360,14 +360,14 @@ UnrealBloomPass.getSeperableBlurMaterial = function(kernelRadius)
 				vec2 invSize = 1.0 / texSize;\n\
 				float fSigma = float(SIGMA);\n\
 				float weightSum = gaussianPdf(0.0, fSigma);\n\
-				vec3 diffuseSum = texture2D( colorTexture, vUv).rgb * weightSum;\n\
+				vec3 diffuseSum = texture2D(colorTexture, vUv).rgb * weightSum;\n\
 				for(int i = 1; i < KERNEL_RADIUS; i++)\n\
 				{\n\
 					float x = float(i);\n\
 					float w = gaussianPdf(x, fSigma);\n\
 					vec2 uvOffset = direction * invSize * x;\n\
-					vec3 sample1 = texture2D( colorTexture, vUv + uvOffset).rgb;\n\
-					vec3 sample2 = texture2D( colorTexture, vUv - uvOffset).rgb;\n\
+					vec3 sample1 = texture2D(colorTexture, vUv + uvOffset).rgb;\n\
+					vec3 sample2 = texture2D(colorTexture, vUv - uvOffset).rgb;\n\
 					diffuseSum += (sample1 + sample2) * w;\n\
 					weightSum += 2.0 * w;\n\
 				}\n\
@@ -425,7 +425,7 @@ UnrealBloomPass.getCompositeMaterial = function(nMips)
 			\n\
 			void main()\n\
 			{\n\
-				gl_FragColor = bloomStrength * ( lerpBloomFactor(bloomFactors[0]) * vec4(bloomTintColors[0], 1.0) * texture2D(blurTexture1, vUv) + \n\
+				gl_FragColor = bloomStrength * (lerpBloomFactor(bloomFactors[0]) * vec4(bloomTintColors[0], 1.0) * texture2D(blurTexture1, vUv) + \n\
 												 lerpBloomFactor(bloomFactors[1]) * vec4(bloomTintColors[1], 1.0) * texture2D(blurTexture2, vUv) + \n\
 												 lerpBloomFactor(bloomFactors[2]) * vec4(bloomTintColors[2], 1.0) * texture2D(blurTexture3, vUv) + \n\
 												 lerpBloomFactor(bloomFactors[3]) * vec4(bloomTintColors[3], 1.0) * texture2D(blurTexture4, vUv) + \n\
