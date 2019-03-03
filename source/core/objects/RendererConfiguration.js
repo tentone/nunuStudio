a29c38aa8db19e0bb54479877b05793e492db855
Tentone <tentone@outlook.com>
2019-03-03 11:24:41 +0000

    Rendering config

--- source/core/objects/RendererConfiguration.js
+++ source/core/objects/RendererConfiguration.js
@@ -9,6 +9,8 @@ function RendererConfiguration()
 {
 	//TODO <ADD CODE HERE>
 
+	this.backend = RendererConfiguration.WEBGL;
+
 	/**
 	 * Antialiasing flag.
 	 *
@@ -34,6 +36,8 @@ function RendererConfiguration()
 	 * @default PCFSoftShadowMap
 	 */
 	this.shadowsType = THREE.PCFSoftShadowMap;
+	
+	this.shadowsAutoUpdate = true;
 
 	/**
 	 * Tone mapping mode.
@@ -59,9 +63,73 @@ function RendererConfiguration()
 	 * @type {Number}
 	 */
 	this.toneMappingWhitePoint = 1.0;
+
+	this.sortObjects = true;
+
+	this.gammaFactor = 2.0;
+
+	this.gammaInput = false;
+
+	this.precision = "highp";
+
+	this.alpha = true;
+
+	this.premultipliedAlpha = true;
+
+	this.preserveDrawingBuffer = false;
+
+	this.powerPreference = "high-performance";
+
+	this.logarithmicDepthBuffer = false;
 }
 
+RendererConfiguration.WEBGL = 1000;
+RendererConfiguration.WEBGL2 = 1001;
+
+RendererConfiguration.prototype.createRenderer = function()
+{
+	if(this.backend = )
+	/*
+	var renderer = new THREE.WebGLRenderer(
+	{
+		canvas: this.canvas,
+		context: context,
+		precision: "highp",
+		alpha: this.alpha,
+		premultipliedAlpha: true,
+		antialias: settings.antialiasing,
+		preserveDrawingBuffer: false,
+		powerPreference: "high-performance",
+		logarithmicDepthBuffer: false
+	});
+
+	renderer.shadowMap.enabled = settings.shadows;
+	renderer.shadowMap.type = settings.shadowsType;
+	renderer.shadowMap.autoUpdate = true;
+	renderer.shadowMap.needsUpdate = false;
+
+	renderer.toneMapping = settings.toneMapping;
+	renderer.toneMappingExposure = settings.toneMappingExposure;
+	renderer.toneMappingWhitePoint = settings.toneMappingWhitePoint;
+
+	renderer.autoClear = false;
+	renderer.autoClearColor = false;
+	renderer.autoClearDepth = false;
+	renderer.autoClearStencil = false;
+
+	renderer.sortObjects = true;
+
+	renderer.gammaFactor = 2;
+	renderer.gammaInput = false;
+	*/
+};
+
 RendererConfiguration.prototype.toJSON = function()
+{
+	//TODO <ADD CODE HERE>
+};
+
+RendererConfiguration.prototype.fromJSON = function(data)
 {
 	//TODO <ADD CODE HERE>
 };
