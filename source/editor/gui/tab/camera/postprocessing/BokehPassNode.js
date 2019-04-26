2356e37e1df973d152f237f2238778c8df924605
Tentone <tentone@outlook.com>
2019-04-26 09:20:51 +0100

    Bokeh step

--- source/editor/gui/tab/camera/postprocessing/BokehPassNode.js
+++ source/editor/gui/tab/camera/postprocessing/BokehPassNode.js
@@ -9,6 +9,7 @@ function BokehPassNode(parent)
 	this.addText("Aperture");
 	this.aperture = new NumberBox(this);
 	this.aperture.size.set(60, 18);
+	this.aperture.setStep(1e-4);
 	this.aperture.setOnChange(function()
 	{
 		Editor.addAction(new ChangeAction(self.pass, "aperture", self.aperture.getValue()));
@@ -19,6 +20,7 @@ function BokehPassNode(parent)
 	this.addText("Focus");
 	this.focus = new NumberBox(this);
 	this.focus.size.set(60, 18);
+	this.focus.setStep(1e-4);
 	this.focus.setOnChange(function()
 	{
 		Editor.addAction(new ChangeAction(self.pass, "focus", self.focus.getValue()));
@@ -29,6 +31,7 @@ function BokehPassNode(parent)
 	this.addText("Max Blur");
 	this.maxblur = new NumberBox(this);
 	this.maxblur.size.set(60, 18);
+	this.maxblur.setStep(1e-4);
 	this.maxblur.setOnChange(function()
 	{
 		Editor.addAction(new ChangeAction(self.pass, "maxblur", self.maxblur.getValue()));
