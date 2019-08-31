208c655b0b3fc859ed5b0c86cd6f9efe0f93832f
tentone <tentone@outlook.com>
2019-08-31 18:41:06 +0100

    Updated builds

--- docs/docs/api.js
+++ docs/docs/api.js
@@ -1,6 +1,7 @@
 YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
+        "AfterimagePass",
         "AmbientLight",
         "AnimationMixer",
         "AnimationTimer",
