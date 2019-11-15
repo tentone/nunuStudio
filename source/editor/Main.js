5d58096924f909f38c18b0b1c406623f64e83fca
tentone <tentone@outlook.com>
2019-11-15 15:12:38 +0000

    Updated spine version

--- source/editor/Main.js
+++ source/editor/Main.js
@@ -52,7 +52,7 @@ include("lib/pson/PSON.min.js");
 include("lib/three-bmfont.js");
 include("lib/cannon.min.js");
 include("lib/leap.min.js");
-include("lib/spine.js");
+include("lib/spine-threejs.js");
 include("lib/opentype.min.js");
 include("lib/chevrotain.min.js");
 
