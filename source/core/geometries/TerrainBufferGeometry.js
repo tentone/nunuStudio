7a12520ff850c47b9ab62cbd7532e7ff1a85d7a1
tentone <tentone@outlook.com>
2020-06-03 15:03:07 +0100

    Derp

--- source/core/geometries/TerrainBufferGeometry.js
+++ source/core/geometries/TerrainBufferGeometry.js
@@ -38,61 +38,70 @@ TerrainBufferGeometry.prototype.generate = function()
 	var widthSegments = this.parameters.widthSegments;
 	var heightSegments = this.parameters.heightSegments;
 
-	var data = this.image.getImageData();
-	// TODO <REMOVE THIS>
-	console.log(data);
-
-	var widthHalf = width / 2;
-	var heightHalf = height / 2;
-
-	var gridX = Math.floor(widthSegments) || 1;
-	var gridY = Math.floor(heightSegments) || 1;
-	var gridX1 = gridX + 1;
-	var gridY1 = gridY + 1;
-
-	var segment_width = width / gridX;
-	var segment_height = height / gridY;
-
-	// Buffers
-	var indices = [];
-	var vertices = [];
-	var normals = [];
-	var uvs = [];
-
-	// Generate vertices, normals and uvs
-	for(var iy = 0; iy < gridY1; iy ++)
+	var self = this;
+	
+	this.image.getImageData(function(data, w, h)
 	{
-		var y = iy * segment_height - heightHalf;
+		// TODO <REMOVE THIS>
+		console.log(data);
 
-		for(var ix = 0; ix < gridX1; ix ++)
+		var widthHalf = width / 2;
+		var heightHalf = height / 2;
+
+		var gridX = Math.floor(widthSegments) || 1;
+		var gridY = Math.floor(heightSegments) || 1;
+		var gridX1 = gridX + 1;
+		var gridY1 = gridY + 1;
+
+		var segment_width = width / gridX;
+		var segment_height = height / gridY;
+
+		// Buffers
+		var indices = [];
+		var vertices = [];
+		var normals = [];
+		var uvs = [];
+
+		// Generate vertices, normals and uvs
+		for(var iy = 0; iy < gridY1; iy ++)
 		{
-			var x = ix * segment_width - widthHalf;
+			var y = iy * segment_height - heightHalf;
 
-			vertices.push(x, 0, y);
-			normals.push(0, 1, 0);
+			for(var ix = 0; ix < gridX1; ix ++)
+			{
+				var x = ix * segment_width - widthHalf;
 
-			uvs.push(ix / gridX);
-			uvs.push(1 - (iy / gridY));
+				vertices.push(x, 0, y);
+				normals.push(0, 1, 0);
+
+				uvs.push(ix / gridX);
+				uvs.push(1 - (iy / gridY));
+			}
 		}
-	}
 
-	// Indices
-	for(var iy = 0; iy < gridY; iy ++)
-	{
-		for(var ix = 0; ix < gridX; ix ++)
+		// Indices
+		for(var iy = 0; iy < gridY; iy ++)
 		{
-			var a = ix + gridX1 * iy;
-			var b = ix + gridX1 * (iy + 1);
-			var c = (ix + 1) + gridX1 * (iy + 1);
-			var d = (ix + 1) + gridX1 * iy;
+			for(var ix = 0; ix < gridX; ix ++)
+			{
+				var a = ix + gridX1 * iy;
+				var b = ix + gridX1 * (iy + 1);
+				var c = (ix + 1) + gridX1 * (iy + 1);
+				var d = (ix + 1) + gridX1 * iy;
 
-			indices.push(a, b, d);
-			indices.push(b, c, d);
+				indices.push(a, b, d);
+				indices.push(b, c, d);
+			}
 		}
-	}
 
-	this.setIndex(indices);
-	this.setAttribute("position", new THREE.Float32BufferAttribute(vertices, 3));
-	this.setAttribute("normal", new THREE.Float32BufferAttribute(normals, 3));
-	this.setAttribute("uv", new THREE.Float32BufferAttribute(uvs, 2));
+		self.setIndex(indices);
+
+		self.setAttribute("position", new THREE.Float32BufferAttribute(vertices, 3));
+		self.setAttribute("normal", new THREE.Float32BufferAttribute(normals, 3));
+		self.setAttribute("uv", new THREE.Float32BufferAttribute(uvs, 2));
+
+		self.attributes.position.needsUpdate = true;
+		self.attributes.normal.needsUpdate = true;
+		self.attributes.uv.needsUpdate = true;
+	});
 };
