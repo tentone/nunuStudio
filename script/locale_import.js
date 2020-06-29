73ffda1caa12d097eae52458de1ac8ed693fe951
tentone <tentone@outlook.com>
2020-06-29 22:35:08 +0100

    Locale import

---
+++ script/locale_import.js
@@ -0,0 +1,36 @@
+// Script to import locale into editor files.
+
+const common = require("./common");
+
+var basePath = "../source/editor";
+
+// Get the files in the path
+var files = [];
+
+
+var path = common.listFiles(basePath, false, "");
+for(var j = 0; j < path.length; j++)
+{
+	files.push(basePath + path[j]);
+}
+
+var localePath = "../source/editor/locale/LocaleManager.js";
+
+
+for(var i = 0; i < files.length; i++)
+{
+	if(files[i].fullPath.search("Locale") >= 0)
+	{
+		continue;
+	}
+
+	var data = common.readFile(files[i]);
+
+	if(files[j].endsWith("js") && data.search("Locale") >= 0)
+	{
+		data = "import {Locale} from \"" + common.calculateRelativePath(files[i], localePath) + "\";\n" + data;
+		modulesImported.push(files[j].className);
+	}
+
+	common.writeFile(files[i], data);
+}
