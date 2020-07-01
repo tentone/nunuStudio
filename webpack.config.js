e398c57ae7450185d74dc4d99295a9dee81e6c39
tentone <tentone@outlook.com>
2020-07-02 00:58:14 +0100

    Test

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const Path = require("path");
 const Webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MergeIntoSingleFilePlugin = require('webpack-merge-and-include-globally');
 const CopyPlugin = require("copy-webpack-plugin");
 
 const context = Path.resolve(__dirname, ".");
@@ -9,7 +10,7 @@ const output = context + "/dist";
 
 module.exports = {
 	context: source,
-	entry: [source + "/editor/Main.js"],
+	entry: source + "/editor/Main.js",
 	target: "web",
 	devtool: "inline-source-map",
 	plugins: [
@@ -30,11 +31,35 @@ module.exports = {
 		new Webpack.ProvidePlugin({
 			THREE: "three",
 			"window.THREE": "three"
-		})
+		}),
+		new MergeIntoSingleFilePlugin({
+			files: {
+				"codemirror.js": [
+					'node_modules/codemirror/bin/codemirror.js',
+					'node_modules/codemirror/keymap/**/*.js',
+					'node_modules/codemirror/addon/**/*.js',
+					'node_modules/codemirror/mode/javascript/**/*.js',
+					'node_modules/codemirror/mode/css/**/*.js',
+					'node_modules/codemirror/mode/htmlmixed/**/*.js',
+					'node_modules/codemirror/mode/xml/**/*.js',
+				],
+				"codemirror.css": [
+					'node_modules/codemirror/bin/codemirror.css',
+					'node_modules/codemirror/theme/**/*.css',
+				]
+			}
+		}),
 	],
-	resolve: {
-		modules: [source, "node_modules"],
-		extensions: [".js"]
+	module: {
+		loaders: [
+		  {
+			test: require.resolve("spine-runtimes/spine-ts/build/spine-threejs.js"),
+			loader: '@shoutem/webpack-prepend-append',
+			query: {
+				append: 'export {spine};'
+			}
+		  }
+		]
 	},
 	output: {
 		filename: "bundle.js",
