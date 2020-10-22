c555f767b8ec25eedb6dec6e0ff4847850f54dfe
tentone <tentone@outlook.com>
2020-10-22 21:54:35 +0100

    Declare pages

--- source/page/src/page/learn/basics/terrain/terrain.page.ts
+++ source/page/src/page/learn/basics/terrain/terrain.page.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
 
 @Component({
-  selector: 'basics-page',
-  templateUrl: './basics.page.html'
+  selector: 'terrain-page',
+  templateUrl: './terrain.page.html'
 })
 export class TerrainPage {}
--- source/page/src/page/learn/learn.module.ts
+++ source/page/src/page/learn/learn.module.ts
@@ -1,9 +1,28 @@
 import {NgModule} from '@angular/core';
 import {BasicsPage} from "./basics/basics/basics.page";
+import {TerrainPage} from "./basics/terrain/terrain.page";
+import {ShadersPage} from "./basics/shaders/shaders.page";
+import {PythonPage} from "./basics/python/python.page";
+import {ArPage} from "./basics/ar/ar.page";
+import {EmbeddingPage} from "./integration/embedding/embedding.page";
+import {CommunicationPage} from "./integration/communication/communication.page";
+import {PlatformerPage} from "./tutorial/platformer/platformer.page";
 
 @NgModule({
 	declarations: [
-		BasicsPage
+		// Basics
+		BasicsPage,
+		ShadersPage,
+		TerrainPage,
+		PythonPage,
+		ArPage,
+
+		// Integration
+		CommunicationPage,
+		EmbeddingPage,
+
+		// Tutorial
+		PlatformerPage,
 	]
 })
 export class LearnModule {}
