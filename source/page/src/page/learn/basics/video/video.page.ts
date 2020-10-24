ed9d43d584c7c01d47118c0f33d6d2d513543c29
tentone <tentone@outlook.com>
2020-10-24 10:53:04 +0100

    Module

--- source/page/src/page/learn/basics/video/video.page.ts
+++ source/page/src/page/learn/basics/video/video.page.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
 
 @Component({
-  selector: 'script-page',
-  templateUrl: './script.page.html'
+  selector: 'video-page',
+  templateUrl: './video.page.html'
 })
 export class VideoPage {}
--- source/page/src/page/learn/learn.module.ts
+++ source/page/src/page/learn/learn.module.ts
@@ -12,6 +12,19 @@ import {VideoPage} from "./basics/video/video.page";
 import {JoystickPage} from "./basics/joystick/joystick.page";
 import {TimelinePage} from "./basics/timeline/timeline.page";
 import {WaterPage} from "./basics/water/water.page";
+import {MinecraftPage} from "./tutorial/minecraft/minecraft.page";
+import {NetworkingPage} from "./tutorial/networking/networking.page";
+import {AnimationPage} from "./basics/animation/animation.page";
+import {AudioPage} from "./basics/audio/audio.page";
+import {CameraPage} from "./basics/camera/camera.page";
+import {GyroscopePage} from "./basics/gyroscope/gyroscope.page";
+import {MaterialsPage} from "./basics/materials/materials.page";
+import {ParticlesPage} from "./basics/particles/particles.page";
+import {PhysicsPage} from "./basics/physics/physics.page";
+import {PostProcessingPage} from "./basics/post-processing/post-processing.page";
+import {RaycasterPage} from "./basics/raycaster/raycaster.page";
+import {TextPage} from "./basics/text/text.page";
+import {VrPage} from "./basics/vr/vr.page";
 
 @NgModule({
 	declarations: [
@@ -26,6 +39,19 @@ import {WaterPage} from "./basics/water/water.page";
 		JoystickPage,
 		TimelinePage,
 		WaterPage,
+		VideoPage,
+		AnimationPage,
+		AudioPage,
+		CameraPage,
+		GyroscopePage,
+		MaterialsPage,
+		ParticlesPage,
+		PhysicsPage,
+		PostProcessingPage,
+		RaycasterPage,
+		TerrainPage,
+		TextPage,
+		VrPage,
 
 		// Integration
 		CommunicationPage,
@@ -33,6 +59,8 @@ import {WaterPage} from "./basics/water/water.page";
 
 		// Tutorial
 		PlatformerPage,
+		MinecraftPage,
+		NetworkingPage
 	]
 })
 export class LearnModule {}
