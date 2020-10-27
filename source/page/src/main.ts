ea16a3b4e7769145476e72a136ca9a0d90960f34
tentone <tentone@outlook.com>
2020-10-27 18:18:26 +0000

    Get jquery and bootstrap from npm

--- source/page/src/main.ts
+++ source/page/src/main.ts
@@ -1,7 +1,5 @@
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {Module} from './module';
-import "jquery/dist/jquery.js";
-import "bootstrap/dist/bootstrap.js";
 import hljs from 'highlight.js/lib/core';
 import javascript from 'highlight.js/lib/languages/javascript';
 import python from 'highlight.js/lib/languages/python';
