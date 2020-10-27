25abef114582fdf3e1f2f176363c21b9a3c0e58b
tentone <tentone@outlook.com>
2020-10-27 16:45:34 +0000

    xml tags

--- source/page/src/main.ts
+++ source/page/src/main.ts
@@ -2,8 +2,12 @@ import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {Module} from './module';
 import hljs from 'highlight.js/lib/core';
 import javascript from 'highlight.js/lib/languages/javascript';
+import python from 'highlight.js/lib/languages/python';
+import xml from 'highlight.js/lib/languages/xml';
 
 hljs.registerLanguage('javascript', javascript);
+hljs.registerLanguage('xml', xml);
+hljs.registerLanguage('python', python);
 hljs.initHighlightingOnLoad();
 
 platformBrowserDynamic().bootstrapModule(Module);
