120992b113cf75fe08f66818e7ea5d2dc60414b2
tentone <tentone@outlook.com>
2020-10-28 22:26:06 +0000

    Derp

--- source/page/src/components/app-viewer/app-viewer.component.ts
+++ source/page/src/components/app-viewer/app-viewer.component.ts
@@ -1,11 +1,20 @@
-import {Component, ElementRef, Input, OnDestroy, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
+import {
+	AfterViewChecked,
+	Component,
+	ElementRef,
+	Input,
+	OnDestroy,
+	OnInit,
+	ViewChild,
+	ViewEncapsulation
+} from '@angular/core';
 
 @Component({
 	selector: 'app-viewer',
 	templateUrl: 'app-viewer.component.html',
 	encapsulation: ViewEncapsulation.None
 })
-export class AppViewerComponent implements OnInit, OnDestroy {
+export class AppViewerComponent implements OnInit, OnDestroy, AfterViewChecked {
 	@ViewChild('canvas', {static: true}) public canvas: ElementRef;
 
 	@Input() public fname: string;
@@ -18,81 +27,24 @@ export class AppViewerComponent implements OnInit, OnDestroy {
 	public ngOnInit(): void {
 		// @ts-ignore
 		this.app = new Nunu.App(this.canvas.nativeElement);
-		this.app.loadRunProgram(this.fname, () => {
-			// Fullscreen Button
-			const fs = this.createButton("Fullscreen");
-			fs.onclick = () => {
-				this.app.toggleFullscreen();
-			};
-			this.canvas.nativeElement.parentElement.appendChild(fs);
+		this.app.loadRunProgram(this.fname);
+	}
 
-			// VR Button
-			setTimeout(() =>
-			{
-				if (this.app.vrAvailable())
-				{
-					const vr = this.createButton("Enter VR");
-					vr.onclick = () => {
-						this.app.toggleVR();
-					};
-					this.canvas.nativeElement.parentElement.appendChild(vr);
-				}
-			}, 1000);
-		});
-
-		this.resize();
-		this.canvas.nativeElement.onresize = () => {
-			this.resize();
-		};
+	public ngAfterViewChecked(): void {
+		this.app.resize();
 	}
 
 	public ngOnDestroy(): void {
 		this.app.exit();
 	}
 
-	public resize(): void {
-		// @ts-ignore
-		if (Nunu.Nunu.isFullscreen()) {
-			this.canvas.nativeElement.width = window.innerWidth;
-			this.canvas.nativeElement.height = window.innerHeight;
-		} else {
-			this.canvas.nativeElement.width = this.canvas.nativeElement.offsetWidth;
-			this.canvas.nativeElement.height = this.canvas.nativeElement.offsetHeight;
+	public toggleVR(): void {
+		if (this.app.vrAvailable()) {
+			this.app.toggleVR();
 		}
-
-		this.app.resize();
 	}
 
-	public createButton(text): HTMLElement {
-		const button = document.createElement("div");
-		button.style.backgroundColor = "var(--dark)";
-		button.style.color = "var(--white)";
-		button.style.height = "30px";
-		button.style.lineHeight = "30px";
-		button.style.borderRadius = "5px";
-		button.style.marginTop = "2px";
-		button.style.marginLeft = "40%";
-		button.style.width = "20%";
-		button.style.verticalAlign = "middle";
-		button.style.textAlign = "center";
-		button.style.cursor = "pointer";
-
-		const span = document.createElement("span");
-		button.appendChild(span);
-
-		const b = document.createElement("b");
-		b.innerHTML = text;
-		span.appendChild(b);
-
-		button.onmouseenter = function()
-		{
-			button.style.backgroundColor = "var(--primary)";
-		};
-		button.onmouseleave = function()
-		{
-			button.style.backgroundColor = "var(--dark)";
-		};
-
-		return button;
+	public toggleFullscreen(): void {
+		this.app.toggleFullscreen();
 	}
 }
