9911a278294227e5f0c9491d6733d355d070ef0d
tentone <tentone@outlook.com>
2020-12-24 16:39:04 +0000

    Text SDF

---
+++ source/core/objects/text/TextSDF.js
@@ -0,0 +1,15 @@
+import {Text} from "troika-three-text"
+
+function TextSDF() {
+	Text.call(this);
+
+	this.text = "text";
+	this.fontSize = 0.1
+	this.color = 0xFFFFFF;
+	this.anchorX = "center";
+	this.anchorY = "middle";
+	this.rotation.set(Math.PI, Math.PI, Math.PI);
+	this.sync();
+}
+
+TextSDF.prototype = Object.create(Text.prototype);
