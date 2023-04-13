9979d7516fd8199998fb28abc3ac63812868a1d3
Tentone <tentone@outlook.com>
2023-04-13 09:17:35 +0100

    Github page

--- source/page/src/global.ts
+++ source/page/src/global.ts
@@ -1,6 +1,8 @@
 export class Global {
 	public static editor: string = 'https://www.nunustudio.org/editor/index.html';
 
+	public static repo: string = 'https://github.com/tentone/nunuStudio';
+
 	public static openEditor(fname?: string): void {
 		window.open(fname ? Global.editor + '?nsp=../' + fname : Global.editor);
 	}
